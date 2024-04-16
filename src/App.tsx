c8801fbb4add0b7a41b867a7d647f431eb362890
Corneliu Nastas <cnastas@genevamedia.com>
2024-04-16 23:37:06 +0300

    feat: Initialize React project.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-amber-900 h-screen">
+      <div className="text-amber-400 text-9xl text-center">Hello world</div>
+    </div>
+  );
+}
+
+export default App;
