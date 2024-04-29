88439cf727ce6974505f7b0425d35ab27efb8467
Corneliu Nastas <cnastas@genevamedia.com>
2024-04-29 12:16:03 +0300

    add: local storage impl to searchbar.

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,35 +1,87 @@
-import React, { useCallback, useState } from 'react';
-import { TextField, Box } from '@mui/material';
+import React, { useCallback, useEffect, useState } from 'react';
+import {
+  Autocomplete,
+  TextField,
+  Box,
+  ListItemText,
+  ListItem,
+  ListItemSecondaryAction,
+  IconButton,
+} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 import { ISearchProps } from './Search.types';
 
 const SearchBar: React.FC<ISearchProps> = ({ onSearch }) => {
   const [city, setCity] = useState<string>('');
+  const [history, setHistory] = useState<string[]>([]);
 
-  const handleSearch = useCallback((): void => {
+  useEffect(() => {
+    const savedHistory = localStorage.getItem('searchHistory');
+    if (savedHistory) {
+      setHistory(JSON.parse(savedHistory));
+    }
+  }, []);
+
+  const handleSearch = useCallback(() => {
     if (city) {
       onSearch(city);
+      // @ts-ignore
+      const updatedHistory = [...new Set([city, ...history])];
+      setHistory(updatedHistory);
+      localStorage.setItem('searchHistory', JSON.stringify(updatedHistory));
     }
-  }, [city, onSearch]);
+  }, [city, history, onSearch]);
 
-  const handleKeyPress = useCallback(
-    (event: React.KeyboardEvent): void => {
-      if (event.key === 'Enter' && city) {
-        handleSearch();
-      }
-    },
-    [handleSearch, city],
-  );
+  const removeFromHistory = useCallback((option: string) => {
+    const savedHistory = localStorage.getItem('searchHistory');
+    if (savedHistory) {
+      const historyArray = JSON.parse(savedHistory);
+      const updatedHistory = historyArray.filter((item: string) => item !== option);
+      localStorage.setItem('searchHistory', JSON.stringify(updatedHistory));
+      setHistory(updatedHistory);
+    }
+  }, []);
 
   return (
     <Box display="flex" justifyContent="center" my={4}>
-      <TextField
-        label="Enter a city"
-        variant="outlined"
+      <Autocomplete
+        freeSolo
+        options={history}
         value={city}
-        onChange={(e) => setCity(e.target.value)}
-        onKeyPress={handleKeyPress}
+        onInputChange={(event, newInputValue) => {
+          setCity(newInputValue);
+        }}
+        onChange={(_, newValue) => {
+          // @ts-ignore
+          if (!newValue) return;
+          setCity(newValue);
+          handleSearch();
+        }}
         fullWidth
-        sx={{ width: '100%', mr: 1 }}
+        renderOption={(props, option, state) => (
+          <ListItem {...props}>
+            <ListItemText primary={option} />
+            <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="delete" onClick={(e) => removeFromHistory(option)}>
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        )}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Enter a city"
+            variant="outlined"
+            fullWidth
+            onKeyPress={(event) => {
+              if (event.key === 'Enter') {
+                handleSearch();
+              }
+            }}
+            sx={{ width: '100%' }}
+          />
+        )}
       />
     </Box>
   );
